d1a3c3379ca868d4890c203941099346f85795e9
Portalo <smiguezc01@iesarroyoharnina.es>
2026-01-08 11:37:33 +0100

    properties y orm

--- Properties/src/main/java/conexion/ConexionBBDD.java
+++ Properties/src/main/java/conexion/ConexionBBDD.java
@@ -4,7 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-import config.EnvironmentAccess;
+import config.EnvironmentAccess; 
 
 public class ConexionBBDD {
 	private static ConexionBBDD INSTANCE;
